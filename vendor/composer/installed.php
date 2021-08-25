4c88033ef251828f14d635a417e20de69634f888
mhdighsmi <mhdighsmi.it@gmail.com>
2021-08-26 01:44:45 +0430

    add vendor directoty for namespase composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '73616d7aa133057e1cf3320519b0e5f040a19ccc',
+    'name' => 'soalwp/set-checkou-page',
+  ),
+  'versions' => 
+  array (
+    'soalwp/set-checkou-page' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '73616d7aa133057e1cf3320519b0e5f040a19ccc',
+    ),
+  ),
+);
